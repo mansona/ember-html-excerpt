ecbebc83f97a5d9eec353103dcd8ea5b8c5cc35f
Chris Manson <chris@manson.ie>
2021-04-05 13:59:28 +0100

    add the excerpt helper

---
+++ addon/helpers/excerpt.js
@@ -0,0 +1,58 @@
+import { helper } from '@ember/component/helper';
+import { htmlSafe } from '@ember/string';
+import { isEmpty } from '@ember/utils';
+import downsize from 'downsize-cjs';
+
+export function getExcerpt(html, truncateOptions) {
+  truncateOptions = truncateOptions || {};
+
+  const excerpt = stripHTML(html);
+
+  if (!truncateOptions.words && !truncateOptions.characters) {
+    truncateOptions.words = 50;
+  }
+
+  return downsize(excerpt, truncateOptions);
+}
+
+export function stripHTML(html) {
+  // Strip inline and bottom footnotes
+  let strippedHTML = html.replace(
+    /<a href="#fn.*?rel="footnote">.*?<\/a>/gi,
+    ''
+  );
+  strippedHTML = strippedHTML.replace(
+    /<div class="footnotes"><ol>.*?<\/ol><\/div>/,
+    ''
+  );
+
+  // Strip other html
+  strippedHTML = strippedHTML.replace(/<\/?[^>]+>/gi, '');
+  strippedHTML = strippedHTML.replace(/(\r\n|\n|\r)+/gm, ' ');
+
+  return strippedHTML;
+}
+
+export function excerpt(content, options) {
+  let truncateOptions = options || {};
+  const excerptText = options.custom_excerpt
+    ? String(options.custom_excerpt)
+    : String(content);
+
+  let { words, characters } = truncateOptions;
+  truncateOptions = { words, characters };
+  Object.keys(truncateOptions).map(function (key) {
+    truncateOptions[key] = parseInt(truncateOptions[key], 10);
+  });
+
+  if (!isEmpty(options.custom_excerpt)) {
+    truncateOptions.characters = options.custom_excerpt.length;
+    if (truncateOptions.words) {
+      delete truncateOptions.words;
+    }
+  }
+
+  return htmlSafe(getExcerpt(excerptText, truncateOptions));
+}
+
+export default helper(excerpt);
---
+++ app/helpers/excerpt.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-html-excerpt/helpers/excerpt';
